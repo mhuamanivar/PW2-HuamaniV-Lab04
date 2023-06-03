1bfadc9cf739d5fe524c323de4e1fad5383a10a4
mhuamanivar <93347594+mhuamanivar@users.noreply.github.com>
2023-06-03 15:06:00 -0500

    Clase picture - mirrors

---
+++ propuestos/picture.py
@@ -0,0 +1,25 @@
+from colors import *
+
+class Picture:
+    def __init__(self, img):
+        self.img = img
+
+    def __eq__(self, other):
+        return self.img == other.img
+
+    def _invColor(self, color):
+        if color not in inverter:
+            return color
+        return inverter[color]
+
+    def verticalMirror(self):
+        """ Devuelve el espejo vertical de la imagen """
+        vertical = []
+        for value in self.img:
+            vertical.append(value[::-1])
+        return Picture(vertical)
+
+    def horizontalMirror(self):
+        """ Devuelve el espejo horizontal de la imagen """
+        horizontal = self.img[::-1]
+        return Picture(horizontal)

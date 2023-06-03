d56f298b0d3bd8ae73fa979e2e45484bff045ec2
mhuamanivar <93347594+mhuamanivar@users.noreply.github.com>
2023-06-03 17:23:59 -0500

    Función negative y join

--- propuestos/picture.py
+++ propuestos/picture.py
@@ -23,3 +23,19 @@ class Picture:
         """ Devuelve el espejo horizontal de la imagen """
         horizontal = self.img[::-1]
         return Picture(horizontal)
+
+    def negative(self):
+        """ Devuelve un negativo de la imagen """
+        negative = []
+        for value in self.img:
+            invertedColor = "".join([self._invColor(c) for c in value])
+            negative.append(invertedColor)
+        return Picture(negative)
+
+    def join(self, p):
+        """ Devuelve una nueva figura poniendo la figura del argumento 
+            al lado derecho de la figura actual """
+        joined = []
+        for i in range(len(self.img)):
+            joined.append(self.img[i]+p.img[i])
+        return Picture(joined)

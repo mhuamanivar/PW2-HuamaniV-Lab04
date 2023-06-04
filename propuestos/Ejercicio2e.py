17009c80c1f582ba1899700e96fbdb487cff0b66
mhuamanivar <93347594+mhuamanivar@users.noreply.github.com>
2023-06-03 19:10:59 -0500

    Ejercicio2e

---
+++ propuestos/Ejercicio2e.py
@@ -0,0 +1,9 @@
+# Melsy Melany Huamaní Vargas
+# Pw2 - Laboratorio 04
+
+from interpreter import draw
+from chessPictures import *
+
+squares = square.negative().join(square)
+
+draw(squares.horizontalRepeat(4))

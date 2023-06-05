df77b876e2123e6c1dab24eea8f640b898382f4c
mhuamanivar <93347594+mhuamanivar@users.noreply.github.com>
2023-06-04 21:36:13 -0500

    Cambio up

--- propuestos/Ejercicio2g.py
+++ propuestos/Ejercicio2g.py
@@ -13,9 +13,9 @@ bPawns = wPawns.negative()
 wPieces = bSquare.under(rock).join(wSquare.under(knight)).join(bSquare.under(bishop)).join(wSquare.under(queen)).join(bSquare.under(king)).join(wSquare.under(bishop)).join(bSquare.under(knight)).join(wSquare.under(rock))
 bPieces = wPieces.negative()
 
-squares1 = wSquare.up(bSquare)
-squares2 = bSquare.up(wSquare)
+squares1 = bSquare.up(wSquare)
+squares2 = wSquare.up(bSquare)
 
 finalSquares = squares1.join(squares2).verticalRepeat(2).horizontalRepeat(4)
 
-draw(bPieces.up(bPawns.up(finalSquares.up(wPawns.up(wPieces)))))
+draw(wPieces.up(wPawns.up(finalSquares.up(bPawns.up(bPieces)))))
